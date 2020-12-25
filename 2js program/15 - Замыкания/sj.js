7af4bba77e3e573adfcc072c20492ec7360b13fd
Aegis020404 <Aegis020404@gmail.com>
2020-12-25 13:44:46 +0300

    Замыкание

--- 2js program/15 - Замыкания/sj.js
+++ 2js program/15 - Замыкания/sj.js
@@ -1,15 +1,46 @@
 var func = function(){
     var i = 10;
-    return function(){
+    return function() {
         return i;
     }
 };
 
-var myFunc = func();
-
 var anotherFunc = function(){
     var i = 20;
-    console.log(myFunc());
-}
+    console.log(func()());//замыкание выведится 10 так как сы обратились к функции func
+};
 
 anotherFunc();
+
+
+
+var counter = (function(num){
+    var count = 0;
+    return function(num){
+        count = num !== undefined ? num : count;
+        return count++;
+    };
+}());
+console.log(counter())//0
+console.log(counter())//1
+console.log(counter())//2
+console.log(counter())//3
+console.log(counter())//4
+console.log(counter())//5
+console.log(counter(100))//100
+console.log(counter())//101
+
+
+var counter = function(num){
+    
+    counter.count = num !== undefined ? num : counter.count;
+    return counter.count++
+};
+console.log(counter(0))//0
+console.log(counter())//1
+console.log(counter())//2
+console.log(counter())//3
+console.log(counter())//4
+console.log(counter())//5
+console.log(counter())//6
+console.log(counter())//7
