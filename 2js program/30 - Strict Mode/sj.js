2436e32bab012eef416b34bc178ee852a4b324b8
Aegis020404 <Aegis020404@gmail.com>
2021-01-22 19:33:48 +0300

    Strict Mode

---
+++ 2js program/30 - Strict Mode/sj.js
@@ -0,0 +1,36 @@
+'use strict'//ставит ошибки там где их нет , но там где код считается плохим
+
+// var obj = {
+//     name:"Alfred"
+// };
+
+// with (obj) {//нельзя использовать в стрик моде
+//     console.log(name);
+// }
+
+// var func = function(){
+//     console.log(this);
+// };
+
+// func();//в стрик моде выводит undefined
+
+// city = "New York";//без объявления переменной будет считать ошибкой
+// console.log(city);
+// console.log(window.city);
+
+// var object = {};
+// Object.defineProperty(object,"gender",{
+//     value:"male",
+//     writable:false,
+// });
+// console.log(object.gender);
+// object.gender = "female";//нельзя даже пробовать изменять значение , так как врайтбл отключен
+// console.log(object.gender);
+
+
+// var octal = 0251;//запрещены 8миричные числа по причине их полной ненадобностью 
+// consile.log(ocatal);
+
+
+
+
