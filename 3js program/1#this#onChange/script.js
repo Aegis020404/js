dd40ce6370f420ce45dce69091be5a81174b26a8
Aegis020404 <Aegis020404@gmail.com>
2021-02-16 18:16:56 +0300

    array+

---
+++ 3js program/1#this#onChange/script.js
@@ -0,0 +1,6 @@
+var val= function(obj,lowobj,hiobj){
+
+    if((obj.value < lowobj) || (obj.value > hiobj )){
+        alert('ERROR');
+    };
+};
---
+++ verstka/script.js
@@ -0,0 +1,10 @@
+
+let name =[
+    'Ваня',
+    'Андрей',
+    'Саша',
+    'Алеся',
+    'Таня',
+];
+console.log(name);
+console.log(name.sort());
