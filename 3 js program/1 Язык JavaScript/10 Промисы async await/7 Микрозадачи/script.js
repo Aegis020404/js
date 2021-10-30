3fb1c455439a688f1dfcf9ad4d5f0cfdfc649d72
Aegis020404 <Aegis020404@gmail.com>
2021-10-30 16:05:13 +0300

    Микрозадачи

---
+++ 3 js program/1 Язык JavaScript/10 Промисы async await/7 Микрозадачи/script.js
@@ -0,0 +1,16 @@
+// Очередь микрозадач
+    let promise = Promise.resolve(new Date);
+
+    promise.then((date) => console.log(`Промис был выполнен ${date}`));
+
+    console.log('сработает быстрее, чем then');
+
+
+// Необработанные ошибки
+    promise = Promise.reject(new Error("Ошибка в промисе!"));
+
+    setTimeout(() => promise.catch(err => console.log('поймана')), 1000);
+
+    // Ошибка в промисе!
+    window.addEventListener('unhandledrejection', event => console.log(event.reason));
+    console.log('git reverst')
