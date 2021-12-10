38c9ae4fc6fdeaf316545954d692fdb87b532998
Aegis020404 <Aegis020404@gmail.com>
2021-12-10 19:36:24 +0300

    Fetch

---
+++ task_learnJs/Получите данные о пользователях GitHub/script.js
@@ -0,0 +1,20 @@
+async function getUsers(names) {
+    let usersAPI = []
+   for(let login of names) {
+    let response = await fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.status != 200 ? null : res.json())
+    .then(res => usersAPI.push(res))
+   }
+    return usersAPI
+  }
+
+  
+
+
+
+
+  
+  let users = await getUsers(['iliakan', 'remy', 'no.such.users']);
+  console.log(users)
+
+  console.log(users[2])
